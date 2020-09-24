501f861a4530dcfd1729efb9993b6f8dd8291b67
Bill Watters <billywatters13@gmail.com>
2020-09-24 17:49:52 +0000

    Added callback function and mocha+ chai testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,21 @@
 const request = require('request')  
-const breed = process.argv.slice(2)
 const url = 'https://api.thecatapi.com/v1/breeds/search' 
-const search = `${url}?q=${breed}`
-request(search,(error, response, body) => { 
-  if (error) {
-    console.log(`Request failed: ${error}`)
-  }
-  const obj = JSON.parse(body) 
-  const data = obj[0]
-  if (data) {
-    console.log(data.description)
-  } else {
-    console.log(`Couldn't find data on: ${breed}`)
-  }
-})
+
+const fetchBreedDescription = (breed, callback) => {
+  const search = `${url}?q=${breed}`
+  request(search,(error, response, body) => { 
+    if (error) {
+      console.log(`Request failed: ${error}`)
+    }
+    const obj = JSON.parse(body) 
+    const data = obj[0]
+    if (data) {
+      callback(null, data.description)
+    } else {
+      callback(`Couldn't find information on: ${breed}`, null)
+    } 
+  }) 
+} 
+module.exports = {
+  fetchBreedDescription
+}
